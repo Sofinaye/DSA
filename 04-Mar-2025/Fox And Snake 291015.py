84be57910e2667ad0d580a08fc1910179a17f90f
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-03-04 14:44:34 +0200

    Created 'Fox And Snake' by A2SV Hub

---
+++ 04-Mar-2025/Fox And Snake 291015.py
@@ -0,0 +1,14 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n, m = list(map(int, input().split()))
+is_last = True
+for i in range(n):
+    if i % 2 == 0:
+        print("#"*m)
+    else:
+        if is_last:
+            print("."*(m-1)+"#")
+            is_last = False
+        else:
+            print("#"+"."*(m-1))
+            is_last = True
