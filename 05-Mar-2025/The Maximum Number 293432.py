7f093d1b1ca878832717fdf04c01ad4827a59e52
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-03-05 19:50:18 +0200

    Created 'The Maximum Number' by A2SV Hub

---
+++ 05-Mar-2025/The Maximum Number 293432.py
@@ -0,0 +1,24 @@
+# Problem: The Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution(object):
+    def thirdMax(self, nums):
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        
+        distinct = []
+        for val in count:
+            distinct.append(val)
+
+        n = len(distinct)
+        distinct.sort()
+
+        return distinct[-3] if n > 2 else distinct[-1]
+
+
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
