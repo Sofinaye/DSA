7a8aa70b4df49749588ebfd261b88cbba7cc1d40
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-04-14 23:12:06 +0300

    Created 'C - z-sort' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/C - z-sort 334016.py
@@ -0,0 +1,18 @@
+# Problem: C - z-sort - https://codeforces.com/gym/603156/problem/C
+
+n = int(input())
+a = list(map(int, input().split(" ")))
+a.sort()
+result = [0] * n
+left = 0
+right = n - 1
+
+for i in range(n):
+    if i % 2 == 0:
+        result[i] = a[left]
+        left += 1
+    else:
+        result[i] = a[right]
+        right -= 1
+
+print(" ".join(map(str, result)))
