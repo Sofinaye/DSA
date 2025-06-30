f87aa926d6ea26de2d420eff35fbeb082bcf0735
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-06-30 20:52:58 +0300

    Created 'B - New Year Transportation' by A2SV Hub

---
+++ A2SV Contest #20 30-Jun-2025/B - New Year Transportation 372112.py
@@ -0,0 +1,13 @@
+# Problem: B - New Year Transportation - https://codeforces.com/gym/618729/problem/B
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+current = 1
+reached = False
+while current < n:
+    next_cell = current + a[current - 1]
+    if next_cell == t:
+        reached = True
+        break
+    current = next_cell
+print("YES" if reached else "NO")
