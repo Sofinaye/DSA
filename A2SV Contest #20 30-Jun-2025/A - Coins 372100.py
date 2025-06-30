cc328dc06ad0d18e23370a1e2ec5fd1e85f916c9
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-06-30 20:42:51 +0300

    Created 'A - Coins' by A2SV Hub

---
+++ A2SV Contest #20 30-Jun-2025/A - Coins 372100.py
@@ -0,0 +1,21 @@
+# Problem: A - Coins - https://codeforces.com/gym/618729/problem/A
+
+t = int(input())
+res = []
+for _ in range(t):
+    n, k = map(int, input().split())
+
+    y = n // k
+
+    remaining = n - y * k
+
+    if remaining % 2 == 0:
+        res.append("YES")
+    else:
+        if y > 0 and (n - k * (y - 1)) % 2 == 0:
+            res.append("YES")
+        else:
+            res.append("NO")
+
+for result in res:
+    print(result)
