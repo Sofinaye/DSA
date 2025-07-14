898d55dc05475c2f0da7faf64fdc390485646e7d
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-07-14 21:08:30 +0300

    Created 'C - The Double-Holed Culprit' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/C - The Double-Holed Culprit 376487.py
@@ -0,0 +1,14 @@
+# Problem: C - The Double-Holed Culprit - https://codeforces.com/gym/622136/problem/C
+
+n = int(input())
+p = list(map(int, input().split()))
+result = []
+for start in range(1, n + 1):
+    visted = [False] * (n + 1)
+    current = start
+    while not visted[current]:
+        visted[current] = True
+        current = p[current - 1]
+
+    result.append(current)
+print(" ".join(map(str, result)))
