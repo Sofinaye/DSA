ddb6509dba134073a4f7db0feb348b546cd4f3bc
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-07-14 20:56:53 +0300

    Created 'B - Lifeboat Lineup' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/B - Lifeboat Lineup 376481.py
@@ -0,0 +1,19 @@
+# Problem: B - Lifeboat Lineup - https://codeforces.com/gym/622136/problem/B
+
+n = int(input())
+crew = []
+for i in range(n):
+    name, status = input().split()
+    crew.append((name, status, i))
+priority_order = {'rat': 0, 'woman': 1, 'child': 1, 'man': 2, 'captain': 3}
+
+crew_with_priority = []
+for member in crew:
+    name, status, original_index = member
+    priority = priority_order[status]
+    crew_with_priority.append((priority, original_index, name))
+
+crew_sorted = sorted(crew_with_priority, key=lambda x: (x[0], x[1]))
+
+for member in crew_sorted:
+    print(member[2])
