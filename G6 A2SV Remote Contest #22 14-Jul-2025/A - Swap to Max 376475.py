59567c0d905b6f69e25807144baf0e15f3888d87
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-07-14 20:46:59 +0300

    Created 'A - Swap to Max' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/A - Swap to Max 376475.py
@@ -0,0 +1,14 @@
+# Problem: A - Swap to Max - https://codeforces.com/gym/622136/problem/A
+
+t = int(input())
+for _ in range(t):
+    l = int(input())
+    A = float('-inf')
+    B = float('-inf')
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    for i in range(l):
+        A = max(A, max(a[i], b[i]))
+        B = max(B, min(a[i], b[i]))
+
+    print(A * B)
