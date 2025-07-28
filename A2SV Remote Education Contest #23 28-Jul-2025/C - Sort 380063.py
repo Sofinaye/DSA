970a3afffa5788ce38c8e3c4bd6f6393532855e1
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-07-28 21:47:32 +0300

    Created 'C - Sort' by A2SV Hub

---
+++ A2SV Remote Education Contest #23 28-Jul-2025/C - Sort 380063.py
@@ -0,0 +1,29 @@
+# Problem: C - Sort - https://codeforces.com/gym/625306/problem/C
+
+def solve():
+    t = int(input())
+    results = []
+    for _ in range(t):
+        n, c = map(int, input().split())
+        a = list(map(int, input().split()))
+
+        teleporters = []
+        for i in range(n):
+            teleporters.append((i + 1 + a[i], i + 1, a[i]))
+
+        teleporters.sort()
+
+        total_cost = 0
+        count = 0
+        for cost, pos, a_cost in teleporters:
+            if total_cost + cost <= c:
+                total_cost += cost
+                count += 1
+            else:
+                break
+        results.append(count)
+
+    print('\n'.join(map(str, results)))
+
+
+solve()
