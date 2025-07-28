ff5a034744d0022d66a2036ef0a63af36bbf27fb
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-07-28 20:37:41 +0300

    Created 'A - Verifying Access Keys' by A2SV Hub

---
+++ A2SV Remote Education Contest #23 28-Jul-2025/A - Verifying Access Keys 380019.py
@@ -0,0 +1,32 @@
+# Problem: A - Verifying Access Keys - https://codeforces.com/gym/625306/problem/A
+
+
+def solve():
+    n = int(input())
+    s = input().strip()
+
+    found_letter = False
+    for i in range(n):
+        char = s[i]
+
+        if char.isdigit():
+            if found_letter:
+                print("NO")
+                return
+
+            if i > 0 and s[i-1].isdigit() and char < s[i-1]:
+                print("NO")
+                return
+        else:
+            found_letter = True
+
+            if i > 0 and s[i-1].isalpha() and char < s[i-1]:
+                print("NO")
+                return
+
+    print("YES")
+
+
+t = int(input())
+for _ in range(t):
+    solve()
