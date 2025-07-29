e5f13077150db9f2639584d8ef4598243acf3aed
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-07-29 22:50:03 +0300

    Created 'House Robber' by A2SV Hub

---
+++ 29-Jul-2025/House Robber 380431.py
@@ -0,0 +1,17 @@
+# Problem: House Robber - https://leetcode.com/problems/house-robber/
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        
+        dp = [0] * (n + 1)
+        dp[1] = nums[0]
+        
+        for i in range(2, n + 1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+        
+        return dp[n]
