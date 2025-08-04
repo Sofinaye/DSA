ce73069bc4a982bc36bd3945461a313c7bb9270c
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-08-04 20:50:56 +0300

    Created 'B - Make Product Equal One' by A2SV Hub

---
+++ A2SV Contest #24 04-Aug-2025/B - Make Product Equal One 381867.py
@@ -0,0 +1,25 @@
+# Problem: B - Make Product Equal One - https://codeforces.com/gym/626626/problem/B
+
+n = int(input())
+a = list(map(int, input().split()))
+total = 0
+negatives = 0
+zeros = 0
+
+for num in a:
+    if num < 0:
+        total += -1 - num
+        negatives += 1
+    elif num > 0:
+        total += num - 1
+    else:
+        total += 1
+        zeros += 1
+
+if negatives % 2:
+    if zeros > 0:
+        pass
+    else:
+        total += 2
+
+print(total)
