1e8781720c0f43a3b02567d4ed2b7644490d11fe
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-08-04 20:50:47 +0300

    Created 'A - Life of a Flower' by A2SV Hub

---
+++ A2SV Contest #24 04-Aug-2025/A - Life of a Flower 381866.py
@@ -0,0 +1,25 @@
+# Problem: A - Life of a Flower - https://codeforces.com/gym/626626/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    res = [0 for i in range(n)]
+    res[0] = 1
+
+    height = 1
+    dead = False
+    for i in range(n):
+        if i > 0 and a[i] == 0 and a[i-1] == 0:
+            dead = True
+            break
+    if dead:
+        print(-1)
+    else:
+        for i in range(n):
+            if a[i] == 1:
+                if i > 0 and a[i-1] == 1:
+                    height += 5
+                else:
+                    height += 1
+        print(height)
