e8b1dd18ac18e78b668a80ea4ea54810c6dc6b56
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-08-07 21:39:00 +0300

    Created 'Champagne Tower' by A2SV Hub

---
+++ 07-Aug-2025/Champagne Tower 382622.py
@@ -0,0 +1,18 @@
+# Problem: Champagne Tower - https://leetcode.com/problems/champagne-tower/
+
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        prev_row = [poured]
+        
+        for row in range(1, query_row + 1):
+            cur_row = [0] * (row + 1)
+            for i in range(row):
+                extra = prev_row[i] - 1
+
+                if extra > 0:
+                    cur_row[i] += (extra)/2
+                    cur_row[i + 1] += (extra)/2
+
+            prev_row = cur_row
+
+        return min(1, prev_row[query_glass])
