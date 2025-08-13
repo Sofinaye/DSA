dacf452911a4dafd476eb0e8a9100b141747fc86
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-08-13 19:18:28 +0300

    Created 'E - Zeroing Array' by A2SV Hub

---
+++ A2SV G6 Remote Contest #25 13-Aug-2025/E - Zeroing Array 384631.py
@@ -0,0 +1,14 @@
+# Problem: E - Zeroing Array - https://codeforces.com/gym/628023/problem/E
+
+n = int(input())
+a = list(map(int, input().split()))
+total_sum = sum(a)
+max_a = max(a)
+
+if total_sum % 2 != 0:
+    print("NO")
+else:
+    if max_a > total_sum - max_a:
+        print("NO")
+    else:
+        print("YES")
