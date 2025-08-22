ea2eea0990daea5d6706d95f368f207ecab07e3f
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-08-22 21:03:08 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 22-Aug-2025/Cirno's Perfect Bitmasks Classroom 387858.py
@@ -0,0 +1,25 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+def is_power_of_two(n):
+    return (n & (n - 1)) == 0
+
+
+def main():
+    t = int(input())
+    index = 1
+    results = []
+    for _ in range(t):
+        x = int(input())
+        index += 1
+        if not is_power_of_two(x):
+            y = x & -x
+            results.append(str(y))
+        else:
+            if x == 1:
+                results.append("3")
+            else:
+                results.append(str(x | 1))
+    print("\n".join(results))
+
+
+main()
