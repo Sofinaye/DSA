6647bba9b56914c9b8d751ae7a4981e4d40ace90
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-08-31 13:25:39 +0300

    Created 'Gray Code' by A2SV Hub

---
+++ 31-Aug-2025/Gray Code 390279.py
@@ -0,0 +1,12 @@
+# Problem: Gray Code - https://leetcode.com/problems/gray-code/
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
+        prev = self.grayCode(n-1)
+        result = list(prev)
+        mask = 1 << (n-1)
+        for code in reversed(prev):
+            result.append(code | mask)
+        return result
