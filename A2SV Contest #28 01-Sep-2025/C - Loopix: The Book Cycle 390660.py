80c3bbea498907a7757b7fe770c4475a12599d76
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-09-01 21:58:33 +0300

    Created 'C - Loopix: The Book Cycle' by A2SV Hub

---
+++ A2SV Contest #28 01-Sep-2025/C - Loopix: The Book Cycle 390660.py
@@ -0,0 +1,23 @@
+# Problem: C - Loopix: The Book Cycle - https://codeforces.com/gym/632067/problem/C
+
+q = int(input())
+for _ in range(q):
+    n = int(input())
+    p = list(map(int, input().split()))
+    p = [x-1 for x in p]
+    visited = [False]*n
+    result = [0]*n
+
+    for i in range(n):
+        if not visited[i]:
+            cycle = []
+            x = i
+            while not visited[x]:
+                cycle.append(x)
+                visited[x] = True
+                x = p[x]
+            cycle_len = len(cycle)
+            for node in cycle:
+                result[node] = cycle_len
+
+    print(*result)
