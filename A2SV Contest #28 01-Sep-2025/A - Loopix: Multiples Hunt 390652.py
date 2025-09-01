f98ade84e1a61c46674c23fd1b2571f3e00a8f5b
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-09-01 21:41:10 +0300

    Created 'A - Loopix: Multiples Hunt' by A2SV Hub

---
+++ A2SV Contest #28 01-Sep-2025/A - Loopix: Multiples Hunt 390652.py
@@ -0,0 +1,26 @@
+# Problem: A - Loopix: Multiples Hunt - https://codeforces.com/gym/632067/problem/A
+
+def solve():
+    l, r, k = map(int, input().split())
+    if k == 1:
+        print(r - l + 1)
+        return
+
+    if l == r:
+        if k == 1:
+            print(1)
+        else:
+            print(0)
+
+        return
+
+    if l > (r // k):
+        print(0)
+    else:
+        count = (r // k) - l + 1
+        print(count)
+
+
+t = int(input())
+for _ in range(t):
+    solve()
