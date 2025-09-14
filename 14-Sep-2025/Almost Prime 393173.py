c69c6974bdc5efc74d36fcd39c0bb40b90a115f6
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-09-14 22:53:42 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 14-Sep-2025/Almost Prime 393173.py
@@ -0,0 +1,23 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+def solve(n):
+    count = 0
+    for num in range(1, n+1):
+        distinct_primes = set()
+        temp = num
+        d = 2
+        while d * d <= temp:
+            if temp % d == 0:
+                distinct_primes.add(d)
+                while temp % d == 0:
+                    temp //= d
+            d += 1
+        if temp > 1:
+            distinct_primes.add(temp)
+        if len(distinct_primes) == 2:
+            count += 1
+    return count
+
+
+n = int(input().strip())
+print(solve(n))
