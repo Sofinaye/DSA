0d8e12b2586ca77b4a04dabcc259cee87be8f0df
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-10-02 21:15:40 +0300

    Created 'B - Game of Mathletes' by A2SV Hub

---
+++ A2SV Contest #29 02-Oct-2025/B - Game of Mathletes 399149.py
@@ -0,0 +1,20 @@
+# Problem: B - Game of Mathletes - https://codeforces.com/gym/633600/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int,input().split())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    A, B = 0, n-1
+    count = 0
+    while A < B:
+        s = arr[A] + arr[B]
+        if s == k:
+            count += 1
+            A += 1
+            B -= 1
+        elif s > k:
+            B -= 1
+        elif s < k:
+            A += 1
+    print(count)
