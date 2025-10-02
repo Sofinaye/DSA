246ef23744ea06bc6194a281d1e5d690a880bfad
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-10-02 21:22:12 +0300

    Created 'A - In Search of an Easy Problem' by A2SV Hub

---
+++ A2SV Contest #31 02-Oct-2025/A - In Search of an Easy Problem 399161.py
@@ -0,0 +1,9 @@
+# Problem: A - In Search of an Easy Problem - https://codeforces.com/gym/637748/problem/A
+
+n = int(input())
+res = list(map(int, input().split()))
+
+if 1 in res:
+    print("HARD")
+else:
+    print("EASY")
