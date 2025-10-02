37e31eb390e1b499109b625bef6907bcf1daed96
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-10-02 21:15:27 +0300

    Created 'A - Zhan's Blender' by A2SV Hub

---
+++ A2SV Contest #29 02-Oct-2025/A - Zhan's Blender 399148.py
@@ -0,0 +1,18 @@
+# Problem: A - Zhan's Blender - https://codeforces.com/gym/633600/problem/A
+
+t = int(input().strip())
+results = []
+for _ in range(t):
+    n = int(input().strip())
+    x, y = map(int, input().split())
+    if n == 0:
+        results.append("0")
+    else:
+        if y >= x:
+            seconds = (n + x - 1) // x
+            results.append(str(seconds))
+        else:
+            seconds = (n + y - 1) // y
+            results.append(str(seconds))
+
+print("\n".join(results))
