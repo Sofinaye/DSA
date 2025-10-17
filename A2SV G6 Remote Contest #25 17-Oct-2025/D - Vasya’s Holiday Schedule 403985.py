f4c4c8c6803ea12cb7c5ec014725b0bdf06f9170
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-10-17 15:34:07 +0300

    Created 'D - Vasya’s Holiday Schedule' by A2SV Hub

---
+++ A2SV G6 Remote Contest #25 17-Oct-2025/D - Vasya’s Holiday Schedule 403985.py
@@ -0,0 +1,30 @@
+# Problem: D - Vasya’s Holiday Schedule - https://codeforces.com/gym/628023/problem/D
+
+n = int(input())
+a = list(map(int, input().split()))
+
+dp = [[float('inf')] * 3 for _ in range(n)]
+
+if a[0] == 0:
+    dp[0][0] = 1
+elif a[0] == 1:
+    dp[0][0] = 1
+    dp[0][1] = 0
+elif a[0] == 2:
+    dp[0][0] = 1
+    dp[0][2] = 0
+elif a[0] == 3:
+    dp[0][0] = 1
+    dp[0][1] = 0
+    dp[0][2] = 0
+
+for i in range(1, n):
+    dp[i][0] = 1 + min(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2])
+    if a[i] in [1, 3]:
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2])
+
+    if a[i] in [2, 3]:
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1])
+
+result = min(dp[n - 1][0], dp[n-1][1], dp[n - 1][2])
+print(result)
