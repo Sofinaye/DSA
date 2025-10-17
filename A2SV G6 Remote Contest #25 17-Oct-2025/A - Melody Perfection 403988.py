0972a508c27e1312add4b9368d0c93227b907d4b
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-10-17 15:34:28 +0300

    Created 'A - Melody Perfection' by A2SV Hub

---
+++ A2SV G6 Remote Contest #25 17-Oct-2025/A - Melody Perfection 403988.py
@@ -0,0 +1,17 @@
+# Problem: A - Melody Perfection - https://codeforces.com/gym/628023/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    condition = True
+    for i in range(1, n):
+        diff = abs(a[i] - a[i - 1])
+        if diff != 7 and diff != 5:
+            condition = False
+            break
+
+    if condition:
+        print("YES")
+    else:
+        print("NO")
