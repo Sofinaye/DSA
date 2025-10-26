ed5dfe6227276457ea7b03cda02ac0672963b7be
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-10-26 20:11:14 +0200

    Created 'E - Knapsack 2' by A2SV Hub

---
+++ 26-Oct-2025/E - Knapsack 2 405828.py
@@ -0,0 +1,19 @@
+# Problem: E - Knapsack 2 - https://atcoder.jp/contests/dp/tasks/dp_e
+
+N, W = map(int, input().split())
+items = [tuple(map(int, input().split())) for _ in range(N)]
+
+Vmax = N * 1000  
+INF = 10**18
+dp = [INF] * (Vmax + 1)
+dp[0] = 0
+
+for w, v in items:
+    for val in range(Vmax, v - 1, -1):
+        dp[val] = min(dp[val], dp[val - v] + w)
+
+ans = 0
+for val in range(Vmax + 1):
+    if dp[val] <= W:
+        ans = val
+print(ans)
