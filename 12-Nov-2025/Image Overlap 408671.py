d21a197699dfa5289da839ba1aa0caa77fd49b75
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-11-12 19:40:29 +0200

    Created 'Image Overlap' by A2SV Hub

---
+++ 12-Nov-2025/Image Overlap 408671.py
@@ -0,0 +1,26 @@
+# Problem: Image Overlap - https://leetcode.com/problems/image-overlap/description/
+
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        n = len(img1)
+        ones1 = []
+        ones2 = []
+        
+        for i in range(n):
+            for j in range(n):
+                if img1[i][j] == 1:
+                    ones1.append((i, j))
+                if img2[i][j] == 1:
+                    ones2.append((i, j))
+        
+        vector_count = defaultdict(int)
+        max_overlap = 0
+        
+        for x1, y1 in ones1:
+            for x2, y2 in ones2:
+                dx = x2 - x1
+                dy = y2 - y1
+                vector_count[(dx, dy)] += 1
+                max_overlap = max(max_overlap, vector_count[(dx, dy)])
+        
+        return max_overlap
