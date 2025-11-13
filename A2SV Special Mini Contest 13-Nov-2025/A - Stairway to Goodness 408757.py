da281ad437670ae920363fd98658222b991068a7
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-11-13 15:33:58 +0200

    Created 'A - Stairway to Goodness' by A2SV Hub

---
+++ A2SV Special Mini Contest 13-Nov-2025/A - Stairway to Goodness 408757.py
@@ -0,0 +1,12 @@
+# Problem: A - Stairway to Goodness - https://codeforces.com/gym/608569/problem/A
+
+t = int(input())
+for _ in range(t):
+    l, r = map(int, input().split())
+    result = 0
+    count = 1
+    while l <= r:
+        result += 1
+        l += count
+        count += 1
+    print(result)
