9519effc1bdb830a7c3ce6676af7bf696a497cbc
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-11-13 15:33:42 +0200

    Created 'B - Counting Without Divisibility' by A2SV Hub

---
+++ A2SV Special Mini Contest 13-Nov-2025/B - Counting Without Divisibility 408756.py
@@ -0,0 +1,8 @@
+# Problem: B - Counting Without Divisibility - https://codeforces.com/gym/608569/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    m = (k - 1) // (n - 1)
+    pos = k - m * (n - 1)
+    print(m * n + pos)
