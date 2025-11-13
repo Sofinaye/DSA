dc03119517575d0285f9cf809a5c2fbabad0699f
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-11-13 15:28:59 +0200

    Created 'Chat Order' by A2SV Hub

---
+++ 13-Nov-2025/Chat Order 408754.py
@@ -0,0 +1,16 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+from collections import defaultdict
+import math
+ 
+def solve():
+    x = int(input())
+    ls = [input() for _ in range(x)]
+    mp = defaultdict(int)
+    for i in range(x-1,-1,-1):
+        if mp[ls[i]]==0:
+            print(ls[i])
+        mp[ls[i]] = 1
+    
+ 
+solve()
