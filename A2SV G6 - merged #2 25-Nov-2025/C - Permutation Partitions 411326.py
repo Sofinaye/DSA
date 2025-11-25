16973d02841bff8ee9c4899d4ce6194faf2b248a
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-11-26 01:19:27 +0200

    Created 'C - Permutation Partitions' by A2SV Hub

---
+++ A2SV G6 - merged #2 25-Nov-2025/C - Permutation Partitions 411326.py
@@ -0,0 +1,20 @@
+# Problem: C - Permutation Partitions - https://codeforces.com/gym/649420/problem/C
+
+MOD = 998244353
+
+n, k = map(int, input().split())
+p = list(map(int, input().split()))
+
+arr = [(val, idx) for idx, val in enumerate(p)]
+arr.sort(reverse=True)
+
+top_k_indices = [idx for val, idx in arr[:k]]
+top_k_indices.sort()
+
+max_value = sum(val for val, idx in arr[:k])
+
+ways = 1
+for i in range(1, k):
+    ways = (ways * (top_k_indices[i] - top_k_indices[i-1])) % MOD
+
+print(max_value, ways)
