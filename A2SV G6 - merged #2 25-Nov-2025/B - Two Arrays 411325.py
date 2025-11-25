78dc7ab61410efed565239f8fb2af7ae0f676252
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-11-26 01:19:15 +0200

    Created 'B - Two Arrays' by A2SV Hub

---
+++ A2SV G6 - merged #2 25-Nov-2025/B - Two Arrays 411325.py
@@ -0,0 +1,19 @@
+# Problem: B - Two Arrays - https://codeforces.com/gym/649420/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, T = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    color = []
+    flip = 0
+    for x in arr:
+        if 2 * x == T:
+            color.append(flip)
+            flip = 1 - flip
+        elif 2 * x < T:
+            color.append(0)
+        else:
+            color.append(1)
+
+    print(' '.join(map(str, color)))
