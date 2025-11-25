0bd278d0857b24a5d472b9d31f150cb337b33e4c
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-11-26 01:19:41 +0200

    Created 'A - Greed' by A2SV Hub

---
+++ A2SV G6 - merged #2 25-Nov-2025/A - Greed 411327.py
@@ -0,0 +1,20 @@
+# Problem: A - Greed - https://codeforces.com/gym/649420/problem/A
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+total_cola = sum(a)
+
+max1 = max2 = 0
+for cap in b:
+    if cap >= max1:
+        max2 = max1
+        max1 = cap
+    elif cap > max2:
+        max2 = cap
+
+if max1 + max2 >= total_cola:
+    print("YES")
+else:
+    print("NO")
