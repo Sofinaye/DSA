aecda75374014181e6e3871ad134904d624e5d63
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2025-11-26 21:36:19 +0200

    Created 'A - Xorion and the Peaks' by A2SV Hub

---
+++ A2SV Contest #27 26-Nov-2025/A - Xorion and the Peaks 411366.py
@@ -0,0 +1,20 @@
+# Problem: A - Xorion and the Peaks - https://codeforces.com/gym/630556/problem/A
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    if k > (n - 1) // 2:
+        print(-1)
+        continue
+    
+    res = [0] * n
+    large = n
+    for i in range(k):
+        res[2 * i + 1] = large
+        large -= 1
+    small = 1
+    for i in range(n):
+        if res[i] == 0:
+            res[i] = small
+            small += 1
+    print(' '.join(map(str, res)))
